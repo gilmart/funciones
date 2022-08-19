deb3819cefe2ebf666d193902594720bb68b3ae2
unknown <mgilma018@gmail.com>
2022-08-18 19:00:44 -0500

    taller de funciones tradicionales y flechas, 7 ejercicios

--- funcionesflechas.js
+++ funcionesflechas.js
@@ -1,39 +1,171 @@
+/*
 //1. 
 // funcion tradicional
-function calcularDistanciaHaciaPlanetaNaboo (x1,x2,y1,y2){
- 
-let distancia=  Math.sqrt(((x2-x1)**2 + (y2-y1))**2)
-
-    return distancia
+function calcularDistancia (x1,x2,y1,y2){
+  
+    return (Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2)))
 
 }
-
-console.log(calcularDistanciaHaciaPlanetaNaboo(1,5,1,3))
+//console.log(calcularDistancia(1,5,1,3))
+let distancia = calcularDistancia(1,5,1,3)
+console.log(`la distancia calculada fue:  ${distancia}`)
 
 
 // funcion flecha1
 
-const calcularDistanciaHaciaPlanetaNabooFlecha = (x1,x2,y1,y2) => distancia = Math.sqrt(((x2-x1)**2 + (y2-y1))**2)
+const calcularDistanciaFlecha = (x1,x2,y1,y2) =>  Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2))
 
-console.log (calcularDistanciaHaciaPlanetaNabooFlecha(1,5,1,3))
+let distancia1 = calcularDistanciaFlecha(1,5,1,3)
 
+console.log(`la distancia calculada fue:  ${distancia1}`)
 
-// funcion flecha2
-const calcularDistanciaHaciaPlanetaNabooFlecha2 = (x1,x2,y1,y2) =>{
-    let distancia = Math.sqrt(((x2-x1)**2 + (y2-y1))**2)
-    return `Resultado: ${distancia}`
-}
-    console.log(calcularDistanciaHaciaPlanetaNabooFlecha2(1,5,1,3))
 
 
 //2. 
 
 // funcion tradicional
 
-function recibirClavedeNave(clave){
+//declarando a la funcion
+function obtenerNombre(codigo){
+    let nombre = codigo.split(':')[1] + codigo.split(':')[0]
 
+    return nombre
+}
+
+// llamando a la funcion
+
+let resultado = obtenerNombre('11012345 : Gilma')
+console.log(resultado)
+
+// funcion flecha
+
+obtenerNombre1 = codigo1 => nombre = codigo1.split(':')[1] + codigo1.split(':')[0]
+
+let resultado1 = obtenerNombre1('11012345 : Gilma') 
+console.log(resultado1)
+
+//3. 
+
+// funcion tradicional
+//declarando a la funcion
+function calcularTemperatura(tempMax , tempMin){
+    return (tempMax+tempMin)/2
+}
+
+let resultadoTemp = calcularTemperatura(20,13)
+console.log(`la temperatura media es ${resultadoTemp}`)
+
+//funcion flecha
+calcularTemperatura1 = (tempMax,tempMin) => (tempMax+tempMin)/2
+// llamando a la funcion
+let resultadoTemp1 = calcularTemperatura1(20,13)
+console.log(`la temperatura media es ${resultadoTemp1}`)
+
+//4. 
+
+// funcion tradicional
+//declarando a la funcion
+function contabilizarSables (sable){
+
+    return sable.filter(x => x <0).length
+
+}
+console.log(contabilizarSables([2,4,-8,5,-6]))
+
+// funcion flecha
+contabilizarSables1 = sable => sable.filter(x=> x <0).length
+let sablesNegativos = contabilizarSables([2,4,-8,5,-6])
+console.log(sablesNegativos)
+
+//5.
+// funcion tradicional
+
+function calcularSalario(licencias){
+    let salario = 3500000
+    let comision = 1500000
+    let impuestos = 0.95    
     
+    return ((salario+(licencias * comision))*impuestos)
+}
+
+console.log(calcularSalario(1))
+
+// funcion flecha
+
+calcularSalario1 = licencias => { salario = 3500000 , comision = 1500000, impuestos = 0.95   
+    
+    return ( (salario+(licencias * comision))*impuestos)
+}
+console.log(calcularSalario1(1))
+
+
+//6.
+// funcion tradicional
+function Padawan(nombre,planeta,edad,estatura){
+    this.nombre = nombre
+    this.planeta = planeta
+    this.edad = edad
+    this.estatura = estatura
+    this.actividad =  function(){
+        if (edad > 15){
+            console.log(resultado = `La actividad asignada es Manejo del sable de luz`)
+        }
+        else{
+            console.log(resultado = `La actividad asignada es: Manejo de la fuerza`)
+        }
+        return resultado
+    }
+
+    this.datosCompletos =function(){
+        return `\n` + this.nombre + ' '+ `\n` + this.planeta + ' ' + `\n` + this.edad + ' ' +`\n`+ this.estatura + ' ' +`\n` + resultado
+    }
+}
+
+
+let padawan1 = new Padawan('juan','martes',16,'1.60')
+
+console.log(padawan1.datosCompletos())
+console.log(padawan1.actividad())
+
+let padawan2 = new Padawan('jose','saturno',10,'1.40')
+console.log(padawan2.datosCompletos())
+console.log(padawan2.actividad())
+
+
+*/
+//7.
+// funcion tradicional
+
+function mensajeExitoso(aleatorio){
+
+   // let exitoso = ("Plano conseguido, listos para despegar")
+    console.log(`Plano conseguido, ¡feliz viaje! ${aleatorio}`)
+}
+
+function mensajeError(aleatorio){
+
+    // let exitoso = ("Plano conseguido, listos para despegar")
+     console.log(`Plano falso conseguido, intente nuevamente con otra secuencia. ${aleatorio}`)
+ }
+
+
+
+function planos(min,max,mensajeCallBack,mensajeCallBackError){
+
+    let aleatorio = Math.floor((Math.random() * (max-min +1))+ min)
+   
+   if(aleatorio <= 10){
+    mensajeCallBack(`Serie de plano: #${aleatorio} , `)
+    return (aleatorio)
+   }
+
+   else{
+
+    mensajeCallBackError(`Serie de plano: #${aleatorio} , `)
+    return (aleatorio)
+
+   }
 
 }
 
-// funcion flecha
+planos(1,10,mensajeExitoso,mensajeError)
